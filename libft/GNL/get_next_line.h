d2d6219ea4d1a7e4ee62b81d388daeab2cf4bdd6
ConsoleGuy <samu2004ssmm@alumnostrazos.net>
2024-08-05 12:46:13 +0200

    uploading gnl with pragma once

--- libft/GNL/get_next_line.h
+++ libft/GNL/get_next_line.h
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# pragma once
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
 # endif
