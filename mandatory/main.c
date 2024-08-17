1609171d8b9a4a260d8666d19b5f1c6c53215a4c
samusanc <samusanc@student.42madrid.com>
2024-08-17 19:52:40 +0200

    sáb 17 ago 2024 19:52:39 CEST by samusanc modules
    BSP
    
    WALLS
    
    LVL_DSG
    
    SECUNDARIO CUANDO ACABE!!!!!!
    
    MAP
    read map
    background grid
    render pipeline-> if not mouse event dont update, if only zoom update x, if is in game mode dont update, etc..;
    point hitboxes -> square;
    point drag and drop;
    point join automatically
    red area join warning?
    
    states?
    move, delete, add?
    save map
    
    make lines more thick
    resolve the color lines problem
    
    test if the rsqrt is faster than the sqrt and if it is implement the sqrt
    
    ======================= end of git description

--- mandatory/main.c
+++ mandatory/main.c
@@ -136,81 +136,6 @@ int	ft_draw_frame(void *p_cub)
 }
 
 
-t_point	get_bounds_max(t_line tmp, t_point max)
-{
-	if (tmp.a.px > max.px)
-		max.px = tmp.a.px;
-	if (tmp.b.px > max.px)
-		max.px = tmp.b.px;
-	if (tmp.a.py > max.py)
-		max.py = tmp.a.py;
-	if (tmp.b.py > max.py)
-		max.py = tmp.b.py;
-	return (max);
-}
-
-t_point	get_bounds_min(t_line tmp, t_point max)
-{
-	if (tmp.a.px < max.px)
-		max.px = tmp.a.px;
-	if (tmp.b.px < max.px)
-		max.px = tmp.b.px;
-	if (tmp.a.py < max.py)
-		max.py = tmp.a.py;
-	if (tmp.b.py < max.py)
-		max.py = tmp.b.py;
-	return (max);
-}
-
-t_line	get_bounds(t_line *segments, unsigned int size)
-{
-	unsigned int	i;
-	t_point			max;
-	t_point			min;
-
-	i = 0;
-	max = point(FLT_MIN, FLT_MIN);
-	min = point(FLT_MAX, FLT_MAX);
-	while (i < size)
-	{
-		max = get_bounds_max(segments[i], max);
-		min = get_bounds_min(segments[i], min);
-		i++;
-	}
-	return (line(max, min));
-}
-
-t_point	remap_point(t_point pt, t_line bounds, t_resolution map_offset, int offset)
-{
-	t_point	result;
-	float	resultx;
-	float	resulty;
-
-	// 									this can be cut and passed as parameter, optimization!!!
-	resultx = (pt.px - bounds.b.px) * (map_offset.width - offset) / (bounds.a.px - bounds.b.px) + offset;
-	resulty = (pt.py - bounds.b.py) * (map_offset.height - offset) / (bounds.a.py - bounds.b.py) + offset;
-	return (point(resultx, resulty));
-}
-
-void	draw_normal(t_line line, t_img *img)
-{
-	t_point	p10;
-	t_point	p0;
-	t_point	p1;
-	t_point	normal;
-
-	p10 = line.a;
-	p10.px = line.b.px - line.a.px;
-	p10.py = line.b.py - line.a.py;
-	normal = normalize(point(-p10.py, p10.px));
-	p0.px = (line.a.px + line.b.px) * 0.5f;
-	p0.py = (line.a.py + line.b.py) * 0.5f;
-	p1.px = p0.px + normal.px * 12.0f;
-	p1.py = p0.py + normal.py * 12.0f;
-	draw_line(p0, p1, img);
-}
-
-
 //=================================================== segment in engine
 
 typedef	struct s_segment
@@ -244,7 +169,8 @@ void	*default_node_free(void *ptr)
 
 //=================================================== segment in engine
 
-//=============================================BSP IN ENGINE
+
+//============================================= Draw circle in mlx_utils
 
 void	horizontal_line(int x0, int y0, int x1, t_img *img)
 {
@@ -267,14 +193,14 @@ void	plot4points(int cx, int cy, int x, int y, t_img *img)
 }
 			
 
-void	draw_circle(int radius, t_img	*img)
+void	draw_circle(int radius, t_img *img, t_point center)
 {
 
 	int	cx;
 	int	cy;
 
-	cx = 100;
-	cy = 100;
+	cx = center.px;
+	cy = center.py;
 
 	int error;
 	int	x;
@@ -301,49 +227,9 @@ void	draw_circle(int radius, t_img	*img)
 
 }
 
-void	draw_circle2(int radius, t_img	*img)
-{
-	float	x;
-	float	y;
-	float	r;
-	float	distance;
+//============================================= Draw circle in mlx_utils
 
-	y = -1;
-	r = 2.0 * radius;
-	while (y < r)
-	{
-			x = 0;
-			while (x <= r)
-			{
-				distance = (r - sqrt((radius - x) * 
-				(radius - x) + (radius - y) * (radius - y)));
-				distance = (int)(distance / radius);
-				distance *= 255;
-				if (distance)
-				{
-					put_pixel(img, color_point(point(x, y), 
-					color_from_rgb(distance, distance, distance)));
-				}
-				x++;
-			}
-			y++;
-	}
-}
-
-
-/*
-for (float y = 0; y < 2.0 * r; y++)
-{
-    for (float x = 0; x < 2 * r; x++)
-    {
-        float deltaX = r - x;
-        float deltaY = r - y;
-        float distance = sqrt(deltaX * deltaX + deltaY * deltaY);
-        color = clamp (r - distance, 0, 1);
-        plot(x, y, color);
-    }
-}
-*/
+//=============================================BSP IN ENGINE
 
 typedef struct s_BSP
 {
@@ -353,6 +239,153 @@ typedef struct s_BSP
 }				t_BSP;
 
 //=============================================BSP IN ENGINE
+
+//============================================= get_bounds in engine
+
+t_point	get_bounds_max(t_line tmp, t_point max)
+{
+	if (tmp.a.px > max.px)
+		max.px = tmp.a.px;
+	if (tmp.b.px > max.px)
+		max.px = tmp.b.px;
+	if (tmp.a.py > max.py)
+		max.py = tmp.a.py;
+	if (tmp.b.py > max.py)
+		max.py = tmp.b.py;
+	return (max);
+}
+
+t_point	get_bounds_min(t_line tmp, t_point max)
+{
+	if (tmp.a.px < max.px)
+		max.px = tmp.a.px;
+	if (tmp.b.px < max.px)
+		max.px = tmp.b.px;
+	if (tmp.a.py < max.py)
+		max.py = tmp.a.py;
+	if (tmp.b.py < max.py)
+		max.py = tmp.b.py;
+	return (max);
+}
+
+t_line	get_bounds(t_list *segments)
+{
+	t_node			*tmp;
+	t_segment		*tmp2;
+	t_point			max;
+	t_point			min;
+
+	tmp = segments->head;
+	max = point(FLT_MIN, FLT_MIN);
+	min = point(FLT_MAX, FLT_MAX);
+	while (tmp)
+	{
+		tmp2 = (t_segment *)tmp->content;
+		max = get_bounds_max(tmp2->segment, max);
+		min = get_bounds_min(tmp2->segment, min);
+		tmp = tmp->next;
+	}
+	return (line(max, min));
+}
+
+//============================================= get_bounds in engine
+
+//============================================= s_map in t_engine
+typedef struct s_map_editor
+{
+	t_list			segments;
+	t_point			screen_center;
+	int				screen_zoom;
+	int				screen_offset;
+	int				screen_real_offset;
+	t_resolution	map_acot;
+	t_line			bounds;
+}				t_map_editor;
+
+t_map_editor	new_map_editor(t_list segments, int screen_offset, t_resolution img_res);
+
+t_map_editor	new_map_editor(t_list segments, int screen_offset, t_resolution img_res)
+{
+	t_map_editor	result;
+
+	result.segments = segments;
+	printf("my: %i, other:%i\n", result.segments.size, segments.size);
+	result.screen_center = point(0, 0);
+	result.screen_zoom = 0;
+	result.screen_offset = screen_offset;
+	result.screen_real_offset = screen_offset + result.screen_zoom;
+	result.map_acot = resolution(img_res.width - result.screen_real_offset, 
+	img_res.height - result.screen_real_offset);
+	result.bounds = get_bounds(&result.segments);
+	return (result);
+}
+
+//============================================= s_map in t_engine
+
+//============================================== draw map editor in mlx_utils
+
+t_point	remap_point(t_point pt, t_line bounds, t_resolution map_offset, int offset)
+{
+	t_point	result;
+	float	resultx;
+	float	resulty;
+
+	printf("\noffset:%i\n", offset);
+	printf("pt:%f, %f\n", pt.px, pt.py);
+	printf("bounds: p1(%f, %f), p2(%f, %f)\n", bounds.a.px, bounds.a.py, bounds.b.px, bounds.b.py);
+	printf("resolution:%i, %i\n\n", map_offset.width, map_offset.height);
+
+	resultx = (pt.px - bounds.b.px) * (map_offset.width - offset) / (bounds.a.px - bounds.b.px) + offset;
+	resulty = (pt.py - bounds.b.py) * (map_offset.height - offset) / (bounds.a.py - bounds.b.py) + offset;
+	return (point(resultx, resulty));
+}
+
+void	draw_normal(t_line line, t_img *img)
+{
+	t_point	p10;
+	t_point	p0;
+	t_point	p1;
+	t_point	normal;
+
+	p10 = line.a;
+	p10.px = line.b.px - line.a.px;
+	p10.py = line.b.py - line.a.py;
+	normal = normalize(point(-p10.py, p10.px));
+	p0.px = (line.a.px + line.b.px) * 0.5f;
+	p0.py = (line.a.py + line.b.py) * 0.5f;
+	p1.px = p0.px + normal.px * 12.0f;
+	p1.py = p0.py + normal.py * 12.0f;
+	draw_line(p0, p1, img);
+}
+
+void	draw_map_editor(t_map_editor map_editor, t_img *img)
+{
+	t_node		*tmp;
+	t_segment	*tmp1;
+	t_point		tmp2;
+	t_point		tmp3;
+
+	tmp = map_editor.segments.head;
+	while (tmp)
+	{
+		printf("%p, %p, %i\n", tmp->content, map_editor.segments.head, map_editor.segments.tail, map_editor.segments.size);
+		/*
+		tmp1 = (t_segment *)tmp->content;
+		tmp2 = tmp1->segment.a;
+		tmp3 = tmp1->segment.b;
+		tmp2 = remap_point(tmp2, map_editor.bounds, map_editor.map_acot, map_editor.screen_real_offset);
+		printf("tmp2 = x:%f, y:%f\n", tmp2.px, tmp2.py);
+		tmp3 = remap_point(tmp3, map_editor.bounds, map_editor.map_acot, map_editor.screen_real_offset);
+		printf("tmp3 = x:%f, y:%f\n", tmp3.px, tmp3.py);
+		draw_line(tmp2, tmp3, img);
+		//draw_normal(line(tmp2, tmp3), img);
+		*/
+		tmp = tmp->next;
+	}
+}
+
+//============================================== draw map editor in mlx_utils
+
 
 int	main(int argc, char **argv)
 {
@@ -375,50 +408,20 @@ int	main(int argc, char **argv)
 	t_list	segments;
 
 	segments = list(NULL);
-	list_push_b(&segments, node(segment(line(point(1,1), point(7,1))), &default_node_free));
-	list_clear(&segments);
-	ft_printf("segments: %p, %p, %d\n", segments.head, segments.tail, segments.size);
-	/*
-	t_line	segments[4];
+	printf("0:head:%p, tail:%p, %i\n", segments.head, segments.tail, segments.size);
+	list_push_b(&segments, node(segment(line(point(1, 1), point(7, 1))), &default_node_free));
+	printf("1:head:%p, tail:%p, %i\n", segments.head, segments.tail, segments.size);
+	list_push_b(&segments, node(segment(line(point(7, 1), point(7, 8))), &default_node_free));
+	printf("2:head:%p, tail:%p, %i\n", segments.head, segments.tail, segments.size);
+	list_push_b(&segments, node(segment(line(point(7, 8), point(1, 8))), &default_node_free));
+	printf("3:head:%p, tail:%p, %i\n", segments.head, segments.tail, segments.size);
+	list_push_b(&segments, node(segment(line(point(1, 8), point(1, 1))), &default_node_free));
+	printf("4:head:%p, tail:%p, %i\n", segments.head, segments.tail, segments.size);
 
-	segments[0] = line(point(1, 1), point(7, 1));
-	segments[1] = line(point(7, 1), point(7, 8));
-	segments[2] = line(point(7, 8), point(1, 8));
-	segments[3] = line(point(1, 8), point(1, 1));
+	t_map_editor map_edit;
 
-	t_line			bounds;
-	int				screen_offset;
-	t_resolution	map_acot;
-
-	screen_offset = 50;
-	map_acot = resolution(cub->tmp->resolution.width - screen_offset, cub->tmp->resolution.height - screen_offset);
-	bounds = get_bounds(segments, 4);
-	printf("BOUNDS: \nmax:%f, %f\nmin: %f, %f\n", bounds.a.px, bounds.a.py, bounds.b.px, bounds.b.py);
-	int i = 0;
-	while (i < 4)
-	{
-		t_point	tmp1;
-		t_point	tmp2;
-
-		tmp1 = segments[i].a;
-		tmp2 = segments[i].b;
-		tmp1 = remap_point(tmp1, bounds, map_acot, 50);
-		tmp2 = remap_point(tmp2, bounds, map_acot, 50);
-		ft_printf("point:%d (%d, %d)\n", (int)i, (int)tmp1.px, (int)tmp1.py);
-		ft_printf("point:%d (%d, %d)\n", (int)i, (int)tmp2.px, (int)tmp2.py);
-		draw_line(tmp1, tmp2, cub->tmp);
-		draw_normal(line(tmp1, tmp2), cub->tmp);
-		i++;
-	}
-	*/
-	int	z;
-
-	z = 0;
-	while (z < 1000)
-	{
-		draw_circle(5, cub->tmp);
-		z++;
-	}
+	map_edit = new_map_editor(segments, 50, cub->tmp->resolution);
+	draw_map_editor(map_edit, cub->tmp);
 	mlx_put_image_to_window(cub->mlx, cub->mlx_win, cub->tmp->img, 0, 0);
 
 	if (!cub)
